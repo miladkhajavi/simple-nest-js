a3bdb59547531b850d6080579c3f95638010e7d3
Milad <m.khajavi@kavano.co>
2021-12-29 09:59:54 +0330

    feat!: validation filter

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,10 @@
 import { TaskStatus } from '../tasks.model';
-
+import { IsEnum, IsOptional } from 'class-validator';
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
   search?: string;
 }
